5a0c90c302348989ee498663c06bf463a5cbc18e
Laimison <laimison1@gmail.com>
2018-12-31 12:11:17 +0000

    backup script prepared

---
+++ backup_notes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Only Mac OS
+uname -s | grep -q ^Darwin || (echo OS not supported; exit 1)
+
+# No root
+whoami | grep -q ^root$ && (echo use this from your user; exit 1)
+
+BACKUP_DRIVE='/Volumes/home$'
+SCRIPT_LOCATION=$(greadlink -f "$0")
+
+# Install rsync 3 if not installed
+ls /usr/local/bin/rsync > /dev/null|| brew install rsync
+
+# Add me to crontab
+crontab -l > /tmp/my_cron
+if ! grep -q $SCRIPT_LOCATION /tmp/my_cron
+then	
+  echo "*/5 * * * * $SCRIPT_LOCATION" >> /tmp/my_cron
+  crontab /tmp/my_cron && /bin/rm -f /tmp/my_cron
+fi
+
+# Do backup
+/usr/local/bin/gtimeout 180 /usr/local/bin/rsync -vaE --progress --delete-after "${HOME}/Library/Group Containers/group.com.apple.notes" "${HOME}/Library/Containers/com.apple.Notes" ${BACKUP_DRIVE}/ 2>&1 | /usr/bin/tee -a /tmp/rsync_notes.log
